ff837795a68568d80b38a64a1a25ad8ecf00bae0
Nathan Lunde-Berry <mr.kequc@gmail.com>
2016-04-16 16:54:04 +0200

    Some quick changes

--- test/db/doc.spec.js
+++ test/db/doc.spec.js
@@ -10,7 +10,7 @@ var nano = require('nano')("http://127.0.0.1:5984/");
 var forced = nano.use(dbName);
 var db = new NanoRecords(nano, dbName);
 
-var complexBody = { third: 'document', num: 11, deep: { hi: "again.", arr: ["some", "values"] } };
+var complexBody = { complex: 'document', num: 11, deep: { hi: "again.", arr: ["some", "values"] } };
 
 function forceUpdate (doc, data, callback) {
   forced.get(doc.body['_id'], (err, body) => {
@@ -20,20 +20,42 @@ function forceUpdate (doc, data, callback) {
 }
 
 describe('db-doc', () => {
+  
   describe('database does not exist', () => {
     beforeEach((done) => {
       nano.db.destroy(dbName, () => { done(); });
     });
     it('create', (done) => {
-      db.doc.create({ hello: 'there!' }, (err, doc) => {
+      db.doc.create(complexBody, (err, doc) => {
         expect(err).to.be.null;
         expect(doc).to.be.ok;
-        expect(doc.body).to.have.all.keys('hello', '_id', '_rev');
-        done();
+        expect(doc.body).to.have.all.keys('complex', 'num', 'deep', '_id', '_rev');
+        expect(doc.body['deep']).to.have.all.keys('hi', 'arr');
+        expect(doc.body['deep']['arr']).to.eql(["some", "values"]);
+        db.doc.get(doc.getId(), (err, gotDoc) => {
+          expect(err).to.be.null;
+          expect(gotDoc).to.be.ok;
+          expect(gotDoc.body).to.eql(doc.body);
+          done();
+        });
       });
     });
-    it('updateOrCreate');
+    it('updateOrCreate', (done) => {
+      db.doc.updateOrCreate("fake-id-doesnt-exist", { another: 'one' }, (err, doc) => {
+        expect(err).to.be.null;
+        expect(doc).to.be.ok;
+        expect(doc.body).to.have.all.keys('another', '_id', '_rev');
+        expect(doc.getId()).to.equal("fake-id-doesnt-exist");
+        db.doc.get(doc.getId(), (err, gotDoc) => {
+          expect(err).to.be.null;
+          expect(gotDoc).to.be.ok;
+          expect(gotDoc.body).to.eql(doc.body);
+          done();
+        });
+      });
+    });
   });
+  
   describe('database exists', () => {
     before((done) => {
       nano.db.destroy(dbName, () => {
@@ -41,47 +63,59 @@ describe('db-doc', () => {
       });
     });
     it('create', (done) => {
-      db.doc.create({ second: 'document', num: 666 }, (err, doc) => {
-        expect(err).to.be.null;
-        expect(doc).to.be.ok;
-        expect(doc.body).to.have.all.keys('second', 'num', '_id', '_rev');
-        done();
-      });
-    });
-    it('create complex', (done) => {
       db.doc.create(complexBody, (err, doc) => {
         expect(err).to.be.null;
         expect(doc).to.be.ok;
-        expect(doc.body).to.have.all.keys('third', 'num', 'deep', '_id', '_rev');
+        expect(doc.body).to.have.all.keys('complex', 'num', 'deep', '_id', '_rev');
         expect(doc.body['deep']).to.have.all.keys('hi', 'arr');
         expect(doc.body['deep']['arr']).to.eql(["some", "values"]);
-        done();
+        db.doc.get(doc.getId(), (err, gotDoc) => {
+          expect(err).to.be.null;
+          expect(gotDoc).to.be.ok;
+          expect(gotDoc.body).to.eql(doc.body);
+          done();
+        });
       });
     });
+    
     describe('document does not exist', () => {
+      beforeEach((done) => {
+        db.doc.destroy("fake-id-doesnt-exist", () => { done(); })
+      });
       it('get', (done) => {
-        db.doc.get("fake-id-doesnt-exist-0", (err, doc) => {
+        db.doc.get("fake-id-doesnt-exist", (err, doc) => {
           expect(err).to.be.ok;
           expect(doc).to.be.undefined;
           done();
         });
       });
       it('update', (done) => {
-        db.doc.update("fake-id-doesnt-exist-1", { blah: 'will fail' }, (err, doc) => {
+        db.doc.update("fake-id-doesnt-exist", { blah: 'will fail' }, (err, doc) => {
           expect(err).to.be.ok;
           expect(doc).to.be.undefined;
           done();
         });
       });
-      it('updateOrCreate');
+      it('updateOrCreate', (done) => {
+        db.doc.updateOrCreate("fake-id-doesnt-exist", complexBody, (err, doc) => {
+          expect(err).to.be.null;
+          expect(doc).to.be.ok;
+          expect(doc.body).to.have.all.keys('complex', 'num', 'deep', '_id', '_rev');
+          expect(doc.body['deep']).to.have.all.keys('hi', 'arr');
+          expect(doc.body['deep']['arr']).to.eql(["some", "values"]);
+          expect(doc.getId()).to.equal("fake-id-doesnt-exist");
+          done();
+        });
+      });
       it('destroy', (done) => {
-        db.doc.destroy("fake-id-doesnt-exist-2", (err, doc) => {
+        db.doc.destroy("fake-id-doesnt-exist", (err, doc) => {
           expect(err).to.be.ok;
           expect(doc).to.be.undefined;
           done();
         });
       });
     });
+    
     describe('document exists', () => {
       var _doc;
       beforeEach((done) => {
@@ -100,30 +134,53 @@ describe('db-doc', () => {
         });
       });
       it('update', (done) => {
-        db.doc.update(_doc.getId(), { updated: 'changehere' }, (err) => {
+        db.doc.update(_doc.getId(), { complex: 'document updated', updated: 'changehere' }, (err) => {
           expect(err).to.be.null;
           _doc.retrieveLatest((err) => {
             expect(err).to.be.null;
-            expect(_doc.body).to.have.all.keys('third', 'num', 'deep', 'updated', '_id', '_rev');
+            expect(_doc.body).to.have.all.keys('complex', 'num', 'deep', 'updated', '_id', '_rev');
             expect(_doc.body['deep']).to.have.all.keys('hi', 'arr');
             expect(_doc.body['deep']['arr']).to.eql(["some", "values"]);
-            expect(_doc.body['updated']).to.equal('changehere');
-            done();
+            db.doc.get(doc._getId(), (err, gotDoc) => {
+              expect(err).to.be.null;
+              expect(gotDoc).to.be.ok;
+              expect(gotDoc.body).to.eql(_doc.body);
+              done();
+            });
           });
         });
       });
       it('update retries');
       it('update more than maxTimes should fail');
-      it('updateOrCreate');
+      it('updateOrCreate', (done) => {
+        db.doc.updateOrCreate(_doc.getId(), { complex: 'document updated again', updated: 'changed' }, (err) => {
+          expect(err).to.be.null;
+          _doc.retrieveLatest((err) => {
+            expect(err).to.be.null;
+            expect(_doc.body).to.have.all.keys('complex', 'num', 'deep', 'updated', '_id', '_rev');
+            expect(_doc.body['deep']).to.have.all.keys('hi', 'arr');
+            expect(_doc.body['deep']['arr']).to.eql(["some", "values"]);
+            expect(_doc.body['complex']).to.equal('document updated again');
+            expect(_doc.body['updated']).to.equal('changed');
+            done();
+          });
+        });
+      });
       it('updateOrCreate more than maxTimes should fail');
       it('destroy', (done) => {
-        db.doc.destroy(_doc.getId(), (err) => {
+        let id = _doc.getId()
+        db.doc.destroy(id, (err) => {
           expect(err).to.be.null;
-          done();
+          db.doc.get(id, (err, doc) => {
+            expect(err).to.be.ok;
+            expect(doc).to.be.undefined;
+            done();
+          });
         });
       });
       it('destroy retries');
       it('destroy more than maxTries should fail');
     });
   });
+  
 });
